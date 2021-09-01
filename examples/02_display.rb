c6c0943587bcf3b7e29d52ad39de227166e0419a
Fernando Alvarez Junco <fernando.alvarez.junco@akra.de>
2021-09-01 13:19:41 +0200

    add another variable to the display exercise

--- examples/02_display.rb
+++ examples/02_display.rb
@@ -1,9 +1,11 @@
 require 'byebug'
 
 # use `display idx` to see the values that idx takes
-# use `display` to see the current display expression
-# use `undisplay <number>` to remove the displayed expression
+# use `display foo` to see the values that idx takes
+# use `display` to see the current display expressions
+# use `undisplay <number>` to remove a displayed expression
 def run
+  foo = "some text"
   (1..3).each do |idx|
     debugger
     idx
