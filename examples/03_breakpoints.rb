eb021fcdb6dfdda7801c9bc07a0f1867786493b4
Fernando Alvarez Junco <fernando.alvarez.junco@akra.de>
2021-11-04 15:48:05 +0100

    update line where to set the breakpoint

--- examples/03_breakpoints.rb
+++ examples/03_breakpoints.rb
@@ -2,8 +2,8 @@ require 'byebug'
 
 # use `display idx` to see the value of idx
 # use `display` to see the current list. Note that as idx is not defined yet, so it won't display any value for it yet.
-# use `break 23` to set a breakpoint on line 23
-# note that `break 23 if idx.modulo(3) == 0` does not work, as idx is not defined on this point of execution
+# use `break 25` to set a breakpoint on line 25
+# note that `break 25 if idx.modulo(3) == 0` does not work, as idx is not defined on this point of execution
 # use `condition idx.modulo(3) == 0`
 # use `info breakpoints` to see the current breakpoints
 # use `delete 1` to delete the breakpoint
